bd9a792060f979e6eaab4e1b1f55f8660cec83fe
Pankin_Anton <pankin.anton@gmail.com>
2023-10-22 09:14:15 +0500

    python for netops_task_6.2ab_done_22.10

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,41 @@ even if several points above are not met.
 
 Restriction: All tasks must be done using the topics covered in this and previous chapters.
 """
+
+ip_local_b = [255, 255, 255, 255]
+ip_unassigned = [0, 0, 0, 0]
+
+
+while True:
+    ip = input("Enter an IP address in the format 10.0.1.1: ")
+    ip_correct = False
+    ip_s = ip.split('.')
+    if len(ip_s) == 4:
+        ip_correct = True
+        for i in ip_s:
+            if not i.isdigit():
+                ip_correct = False
+                break
+            oct = int(i)
+            if oct < 0 and oct > 255:
+                ip_correct = False
+                break
+        if ip_correct == False:
+            print("Invalid IP")
+        else:
+            if ip_s[0] in range(1, 223):
+                print ('unicast')
+            elif ip_s[0] in range(224, 239):
+                print ('multicast')
+            elif ip_s == ip_local_b:
+                print ('local broadcast')
+            elif ip_s == ip_unassigned:
+                print ('unassigned')
+            else:
+                print ('unused')
+            break
+    else:
+        print("Invalid IP")
+        break
+
+
